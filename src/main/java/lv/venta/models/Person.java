0d16367ed9344bf87795d517d230e343d821f79e
reinisrm <rreeiinniiss@gmail.com>
2023-06-07 00:44:35 +0300

    Add: new person model class with inheritence

--- src/main/java/lv/venta/models/Person.java
+++ src/main/java/lv/venta/models/Person.java
@@ -3,34 +3,29 @@ package lv.venta.models;
 import java.util.Collection;
 
 import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.Table;
+import jakarta.persistence.MappedSuperclass;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
-@Table(name = "student_table")
-@Entity
+@MappedSuperclass
 @Getter
 @Setter
 @NoArgsConstructor
 @ToString
-public class Student {
-	
+public class Person {
 	@Setter(value = AccessLevel.NONE)
-	@Column(name = "ids")
+	@Column(name = "idpe")
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
-	public long ids;
+	public long idpe;
 	
 	@Column(name = "Name")
 	@NotNull
@@ -41,15 +36,4 @@ public class Student {
 	@Size(min = 3, max = 15)
 	@Column(name = "surname")
 	public String surname;
-	
-	@OneToMany(mappedBy = "student")
-	@ToString.Exclude
-	private Collection<Grade> grades;
-
-	public Student(String name, String surname) {
-		this.name = name;
-		this.surname = surname;
-	}
-
-	
 }
