d4c7cf6727257d9646e248fa18ed0c993f7b01ba
reinisrm <rreeiinniiss@gmail.com>
2023-06-08 19:54:07 +0300

    Add: added some filter functions

--- src/main/java/lv/venta/repos/iGradeRepo.java
+++ src/main/java/lv/venta/repos/iGradeRepo.java
@@ -1,9 +1,15 @@
 package lv.venta.repos;
 
+import java.util.ArrayList;
+
 import org.springframework.data.repository.CrudRepository;
 
 import lv.venta.models.Grade;
 
 public interface iGradeRepo extends CrudRepository<Grade, Long> {
-
+	//TODO atlasit visu studentu atzimes, ja ir zinams studentu id
+	ArrayList<Grade> findByStudentIds(long ids);
+	
+	//TODO atlasit visa kursa atzimes, ja ir zinams kursa nosaukums
+	ArrayList<Grade> findByCourseTitle(String title);
 }
